25709a157a6c785dda4c357c781a22c2b02a079f
Tanvir Singh <tanvir.s2k1@gmail.com>
2025-08-17 17:58:56 +1000

    Changed baseURL

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.54.243.108:5001', // live
+  baseURL: 'http://13.54.51.100:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
